ac0d5ddc21380d7d36c714fa4a90f955edb1aba4
Brandon <65471994+BearMSU@users.noreply.github.com>
2026-08-10 16:32:35 -0400

    Update solution - CodeHub

--- python/7-kyu/find-the-stray-number/find-the-stray-number.py
+++ python/7-kyu/find-the-stray-number/find-the-stray-number.py
@@ -1,6 +1,4 @@
 def stray(arr):
-    if arr[0] != arr[1] and arr[0] != arr[2]:
-        return arr[0]
-    else:
-        unequal = [n for n in arr if n != arr[0]]
-        return unequal[0]
+    for n in arr:
+        if arr.count(n) == 1:
+            return n
