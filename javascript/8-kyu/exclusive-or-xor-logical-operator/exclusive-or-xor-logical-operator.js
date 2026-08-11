9a9fc382eec52e6cd21548e56b765af8636d30f4
Brandon <65471994+BearMSU@users.noreply.github.com>
2026-08-11 14:14:36 -0400

    Update solution - CodeHub

--- javascript/8-kyu/exclusive-or-xor-logical-operator/exclusive-or-xor-logical-operator.js
+++ javascript/8-kyu/exclusive-or-xor-logical-operator/exclusive-or-xor-logical-operator.js
@@ -1,8 +1,4 @@
 function xor(a, b) {
   // TODO: Program Me
-  if (a === true && b === false || a === false && b === true) {
-    return true;
-  } else {
-    return false;
-  }
+  return a !== b;
 }
