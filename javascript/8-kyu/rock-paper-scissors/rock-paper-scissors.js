5b1c563d94cdb48a6f1a81c9962f94eb63f8ae2a
Brandon <65471994+BearMSU@users.noreply.github.com>
2026-08-12 14:15:08 -0400

    Update solution - CodeHub

--- javascript/8-kyu/rock-paper-scissors/rock-paper-scissors.js
+++ javascript/8-kyu/rock-paper-scissors/rock-paper-scissors.js
@@ -1,32 +1,13 @@
 const rps = (p1, p2) => {
-  const options = ['rock', 'paper', 'scissors'];
-  const draw = 'Draw!';
-  const p1Wins = 'Player 1 won!'
-  const p2Wins = 'Player 2 won!'
+  const rules = {rock: 'scissors', paper: 'rock', scissors: 'paper'};
   
-  if (p1 === options[0]) {
-    if (p2 === options[0]) {
-      return draw;
-    } else if (p2 === options[1]) {
-      return p2Wins;
-    } else {
-      return p1Wins;
-    }
-  } else if (p1 === options[1]) {
-    if (p2 === options[0]) {
-      return p1Wins;
-    } else if (p2 === options[1]) {
-      return draw;
-    } else {
-      return p2Wins;
-    }
+  if (p1 === p2) {
+    return 'Draw!';
+  }
+  
+  if (p2 === rules[p1]) {
+    return 'Player 1 won!'
   } else {
-    if (p2 === options[0]) {
-      return p2Wins;
-    } else if (p2 === options[1]) {
-      return p1Wins;
-    } else {
-      return draw;
-    }
+    return 'Player 2 won!'
   }
 };
