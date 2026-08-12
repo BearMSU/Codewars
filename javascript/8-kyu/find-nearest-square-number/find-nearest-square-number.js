5efc929323b527cdd9fe1edae4161796570cf343
Brandon <65471994+BearMSU@users.noreply.github.com>
2026-08-12 13:42:24 -0400

    Update solution - CodeHub

--- javascript/8-kyu/find-nearest-square-number/find-nearest-square-number.js
+++ javascript/8-kyu/find-nearest-square-number/find-nearest-square-number.js
@@ -1,15 +1,4 @@
 function nearestSq(n){
     // your code
-    const x = Math.floor(Math.sqrt(n)) ** 2;
-    const y = Math.ceil(Math.sqrt(n)) ** 2;
-  
-    if (n === 1 || n === 2) {
-      return 1;
-    } else if (n === 3) {
-      return 2;
-    } else if ((n - x) > (y - n)) {
-      return y;
-    } else {
-      return x;
-    }
+  return Math.round(Math.sqrt(n), 0) ** 2;
 }
