f5ccad70ff63411c4bdab3850e66db1f9be8e20c
Brandon <65471994+BearMSU@users.noreply.github.com>
2026-09-06 17:50:50 -0400

    Update solution - CodeHub

--- javascript/7-kyu/categorize-new-member/categorize-new-member.js
+++ javascript/7-kyu/categorize-new-member/categorize-new-member.js
@@ -1,11 +1,4 @@
 function openOrSenior(data){
-  let membership = [];
-  for (let i = 0; i < data.length; i++) {
-    if (data[i][0] >= 55 && data[i][1] > 7) {
-      membership.push('Senior');
-    } else {
-      membership.push('Open');
-    }
-  }
-  return membership;
+  // ...
+  return data.map(([age, handicap]) => (age > 54 && handicap > 7) ? 'Senior' : 'Open');
 }
